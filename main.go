8ecc12427bf72b72119842e3100169da0dc2df60
Max Sargent <maxsargentdev@gmail.com>
2022-08-19 10:09:35 +0100

    Add more exercises

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	tutorial "github.com/maxsargentdev/the-go-programming-language/1-tutorial"
-)
+import tutorial "github.com/maxsargentdev/the-go-programming-language/1-tutorial"
 
 func main() {
 	//tutorial.Echo1()
 	//tutorial.Echo2()
 	//tutorial.Echo3()
+	//tutorial.Dup2()
 	//tutorial.Lissajous1(os.Stdout)
 	//tutorial.Lissajous2(os.Stdout)
-	tutorial.Dup2()
+	//tutorial.Fetch1()
+	//tutorial.Fetch2()
+	tutorial.Fetch3()
 }
