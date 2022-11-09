2aa53e2cef14d8d896e9b864caddf838d4459594
Max Sargent <maxsargentdev@gmail.com>
2022-11-09 18:58:50 +0000

    [c2e4] Add additional call to popcount for comparison

--- cmd/c2e4.go
+++ cmd/c2e4.go
@@ -21,6 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		popcount.PopCount(1)
 		popcount.LoopPopCount(1)
 		popcount.BitMaskPopCount(1)
 	},
