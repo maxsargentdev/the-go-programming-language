9259f2cea7bfa40bbf069be1ba48055e3d947011
Max Sargent <maxsargentdev@gmail.com>
2023-02-01 06:57:58 +0000

    [c5e7] Start writing HTML pretty printer

--- cmd/c5e7.go
+++ cmd/c5e7.go
@@ -1,14 +1,15 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"the-go-programming-language/internal/functions/functionvalues"
 
 	"github.com/spf13/cobra"
 )
+
+var c5e7URL string
 
 // c5e7Cmd represents the c5e7 command
 var c5e7Cmd = &cobra.Command{
@@ -21,12 +22,13 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("c5e7 called")
+		functionvalues.RunHTMLPrettyPrint(c5e7URL)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(c5e7Cmd)
+	c5e7Cmd.Flags().StringVarP(&c5e7URL, "url", "u", "https://xkcd.com", "the url to use for running pretty print")
 
 	// Here you will define your flags and configuration settings.
 
